3cc7579fe5d20efc55ed7414df9c8b09fa9ca0dd
Oláh Zoltán <olah.zoltan1k84@gmail.com>
2022-03-01 23:51:55 +0200

    Add Bootstrap and bugfixes

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,12 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "welcome#index"
+
+  #resources :users
+  #delete 'sign_out', to: 'devise/sessions#destroy'
+
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+
 end
