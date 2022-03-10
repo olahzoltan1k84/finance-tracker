f7746da665251bab6efdfb187a2e330742c8df89
Oláh Zoltán <olah.zoltan1k84@gmail.com>
2022-03-10 22:07:20 +0200

    Setup JavaScript response

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,8 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+//= require bootstrap-sprockets
+
 import "@hotwired/turbo-rails"
 import "controllers"
-import * as bootstrap from "bootstrap"
+
+import "popper"
+import "bootstrap"
